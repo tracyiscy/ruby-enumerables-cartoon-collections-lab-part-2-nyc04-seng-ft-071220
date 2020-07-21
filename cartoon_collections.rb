cb915494ef7094e2a54529800607ffef31ef67aa
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 13:06:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-    string.select{|string|
+    planeteer_calls.select{|string|
       string.length>4
     }
 
