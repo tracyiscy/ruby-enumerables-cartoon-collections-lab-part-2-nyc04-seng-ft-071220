8093dac333a7b5a342a68601ab3646273d44befd
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 13:54:45 +0000

    Automatically backed up by Learn

--- demo.rb
+++ demo.rb
@@ -3,7 +3,11 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls.any?{|index|
-    puts planeteer_calls==index
+    if planeteer_calls==index
+      planeteer_calls
+    else
+      nil
+    end
   }
 end
 
