a350dfa8c32c3620f7f1fe76e6e5eb3feead7d9a
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 13:55:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,11 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls.any?{
-    |index| index==planeteer_calls
+    |index| 
+    if index==planeteer_calls
+      index
+    else
+      nil
+    end
   }
 end
