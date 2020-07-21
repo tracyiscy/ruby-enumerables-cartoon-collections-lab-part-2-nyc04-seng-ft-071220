88a178ec3c0f5ddf23b021b1ce40f15f7b7a410a
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 13:55:00 +0000

    Automatically backed up by Learn

--- demo.rb
+++ demo.rb
@@ -4,7 +4,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls.any?{|index|
     if planeteer_calls==index
-      planeteer_calls
+      puts planeteer_calls
     else
       nil
     end
