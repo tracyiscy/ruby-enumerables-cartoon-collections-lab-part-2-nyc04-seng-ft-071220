d7b6c42dd5d1b620b1e393923f37c87e158880e5
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 12:49:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   array.map{|index| index*index
-    array
   }
 end
 
